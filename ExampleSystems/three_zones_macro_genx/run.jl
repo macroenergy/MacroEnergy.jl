611f777524fab46bec15608b57b30ceb403c5227
lbonaldo <bonaldo.luca12@gmail.com>
2024-10-03 19:28:43 -0400

    Fix status.csv in run.jl

--- ExampleSystems/three_zones_macro_genx/run.jl
+++ ExampleSystems/three_zones_macro_genx/run.jl
@@ -21,7 +21,7 @@ println("The objective value for Macro was $(macro_objval)")
 using CSV, DataFrames
 genx_results = joinpath(dirname(@__DIR__), "three_zones_genx", "results")
 if isdir(genx_results)
-    df_genx_status = CSV.read(joinpath(genx_results, "Status.csv"), DataFrame)
+    df_genx_status = CSV.read(joinpath(genx_results, "status.csv"), DataFrame)
     println("The objective value for GenX was $(df_genx_status.Objval[1])")
     println(
         "The relative error between Macro and GenX is $(abs(df_genx_status.Objval[1]-macro_objval)/df_genx_status.Objval[1])",
