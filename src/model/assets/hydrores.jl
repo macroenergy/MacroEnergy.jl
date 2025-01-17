78f33494624bb74f732877d9d26eaaa21b076d42
Filippo Pecci <peccifilippo@gmail.com>
2025-01-17 10:33:31 +0100

    fixed error

--- src/model/assets/hydrores.jl
+++ src/model/assets/hydrores.jl
@@ -67,7 +67,7 @@ function make(::Type{HydroRes}, data::AbstractDict{Symbol,Any}, system::System)
     inflow_edge.can_expand = discharge_edge.can_expand;
     inflow_edge.existing_capacity = discharge_edge.existing_capacity;
     inflow_edge.capacity_size = discharge_edge.capacity_size;
-    inflow_edge.constraints = get(discharge_edge_data, :constraints,[SameChargeDischargeCapacityConstraint();MustRunConstraint()]); 
+    inflow_edge.constraints = get(discharge_edge_data, :constraints,[StorageChargeDischargeRatioConstraint();MustRunConstraint()]); 
 
     hydrostor.discharge_edge = discharge_edge
     hydrostor.charge_edge = inflow_edge
