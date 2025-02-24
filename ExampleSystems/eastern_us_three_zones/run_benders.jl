a013db7033a635699ee3cfbe22a162aca1aa975d
Filippo Pecci <peccifilippo@gmail.com>
2025-02-24 15:28:16 +0100

    testing benders

---
+++ ExampleSystems/eastern_us_three_zones/run_benders.jl
@@ -0,0 +1,45 @@
+### Before running this script, you have to install the MacroEnergySolvers package.
+### To use your local copy, type in the REPL: 
+### using Pkg; Pkg.develop(path="path-to-MacroEnergySolvers.jl")
+
+using MacroEnergy
+using MacroEnergySolvers
+
+system = MacroEnergy.load_system(@__DIR__);
+
+MacroEnergy.scaling!(system)
+
+system_decomp = MacroEnergy.generate_decomposed_system(system);
+
+planning_problem,linking_variables = initialize_planning_problem!(system,MacroEnergy);
+
+number_of_subperiods = length(system_decomp);
+
+subproblems = [Dict() for i in 1:number_of_subperiods];
+
+MacroEnergySolvers.initialize_local_subproblems(system_decomp,subproblems,MacroEnergy);
+
+linking_variables_sub = MacroEnergySolvers.get_local_linking_variables(subproblems);
+
+# start_distributed_processes!(number_of_subperiods,MacroEnergy)
+
+# subproblems_dict,linking_variables_sub = initialize_dist_subproblems!(system_decomp,MacroEnergy)
+
+
+benders_setup = Dict(
+    :solver=>:benders,
+    :MaxIter=> 50,
+    :MaxCpuTime => 7200,
+    :ConvTol => 1e-3,
+    :StabParam => 0.5,
+    :StabDynamic => false,
+    :IntegerInvestment => false
+)
+
+results = benders(planning_problem,linking_variables,subproblems,linking_variables_sub,benders_setup)
+
+# planning_sol = MacroEnergySolvers.solve_planning_problem(planning_problem,linking_variables);
+
+# subop_sol = MacroEnergySolvers.solve_operation_subproblems(subproblems,planning_sol);
+
+println("")
