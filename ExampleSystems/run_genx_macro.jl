58f31962e50c6db3a13637e15bd06468acb34e5a
Filippo Pecci <peccifilippo@gmail.com>
2025-03-03 11:54:25 +0100

    genx testing OK

--- ExampleSystems/run_genx_macro.jl
+++ ExampleSystems/run_genx_macro.jl
@@ -2,9 +2,43 @@ using MacroEnergy
 using Gurobi
 using GenX
 
-genx_case_path = "ExampleSystems/1_three_zones";
-macro_case_path = "ExampleSystems/1_three_zones/1_three_zones_macro";
-GenX.run_genx_case!(genx_case_path,Gurobi.Optimizer)
+genx_case_path = "ExampleSystems/genx_three_zones";
+macro_case_path = "ExampleSystems/genx_three_zones/macro_inputs";
+
+genx_settings = GenX.get_settings_path(genx_case_path, "genx_settings.yml") 
+writeoutput_settings = GenX.get_settings_path(genx_case_path, "output_settings.yml") 
+mysetup = GenX.configure_settings(genx_settings, writeoutput_settings) 
+if mysetup["TimeDomainReduction"] == 1
+    TDRpath = joinpath(genx_case_path, mysetup["TimeDomainReductionFolder"])
+    system_path = joinpath(genx_case_path, mysetup["SystemFolder"])
+    GenX.prevent_doubled_timedomainreduction(system_path)
+    if !GenX.time_domain_reduced_files_exist(TDRpath)
+        println("Clustering Time Series Data (Grouped)...")
+        GenX.cluster_inputs(genx_case_path, settings_path, mysetup)
+    else
+        println("Time Series Data Already Clustered.")
+    end
+end
+### Configure solver
+println("Configuring Solver")
+OPTIMIZER = GenX.optimizer_with_attributes(Gurobi.Optimizer, "BarConvTol"=>1e-8,"Crossover" => 1, "Method" => 2)
+println("Loading Inputs")
+myinputs = GenX.load_inputs(mysetup, genx_case_path)
+println("Generating the Optimization Model")
+time_elapsed = @elapsed EP = GenX.generate_model(mysetup, myinputs, OPTIMIZER)
+println("Time elapsed for model building is")
+println(time_elapsed)
+println("Solving Model")
+EP, solve_time = GenX.solve_model(EP, mysetup)
+# myinputs["solve_time"] = solve_time 
+# println("Writing Output")
+# outputs_path = GenX.get_default_output_folder(genx_case_path)
+# elapsed_time = @elapsed outputs_path = GenX.write_outputs(EP,
+#     outputs_path,
+#     mysetup,
+#     myinputs)
+# println("Time elapsed for writing is")
+# println(elapsed_time)
 
 system = MacroEnergy.load_system(macro_case_path)
 model = MacroEnergy.generate_model(system)
@@ -15,5 +49,7 @@ results_dir = joinpath(macro_case_path , "results")
 mkpath(results_dir)
 MacroEnergy.write_capacity_results(joinpath(results_dir, "capacity.csv"), system)
 MacroEnergy.write_costs(joinpath(results_dir, "costs.csv"), model)
+
+println(abs(GenX.objective_value(EP) - MacroEnergy.objective_value(model))/GenX.objective_value(EP))
 
 println("")
