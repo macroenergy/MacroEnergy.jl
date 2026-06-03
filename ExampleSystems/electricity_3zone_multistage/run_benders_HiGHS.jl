cb027195fbd99c54e06b862d370e55217c9b04db
Emil Dimanchev <emild@mit.edu>
2026-06-03 11:28:39 -0400

    cleanup

--- ExampleSystems/electricity_3zone_multistage/run_benders_HiGHS.jl
+++
@@ -1,9 +0,0 @@
-using MacroEnergy
-using HiGHS
-
-(case, solution) = run_case(@__DIR__;
-    planning_optimizer=HiGHS.Optimizer,
-    subproblem_optimizer=HiGHS.Optimizer,
-    planning_optimizer_attributes=("solver" => "ipm", "run_crossover" => "off", "ipm_optimality_tolerance" => 1e-3),
-    subproblem_optimizer_attributes=("solver" => "ipm", "run_crossover" => "on", "ipm_optimality_tolerance" => 1e-3)
-);
--- ExampleSystems/electricity_3zone_multistage_learning_speedlimits/run_gurobi.jl
+++
@@ -1,7 +0,0 @@
-using MacroEnergy
-using Gurobi
-
-(case, solution) = run_case(@__DIR__;
-    optimizer=Gurobi.Optimizer,
-    optimizer_attributes=("Method" => 2, "Crossover" => 1, "BarConvTol" => 1e-8)
-);
