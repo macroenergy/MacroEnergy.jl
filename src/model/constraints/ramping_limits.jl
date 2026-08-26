a8b9adeee22843de6aadc09f035968dcc0a20b98
Ruaridh Macdonald <rmacd@mit.edu>
2026-08-26 06:51:38 -0400

    Save ramping constraints to constraint_ref

--- src/model/constraints/ramping_limits.jl
+++ src/model/constraints/ramping_limits.jl
@@ -38,18 +38,17 @@ function add_model_constraint!(ct::RampingLimitConstraint, e::EdgeWithoutUC, mod
         # reserves_term = @expression(model, [t in time_interval(e)], container = array_container(time_interval(e)), 0 * model[:vREF])
         # regulation_term = @expression(model, [t in time_interval(e)], container = array_container(time_interval(e)), 0 * model[:vREF])
 
-        @constraint(
+        ct.constraint_ref = @constraint(
             model,
-            [t in ti],
-            flow(e, t) - flow(e, timestepbefore(t, 1, sps)) -
-            ramp_up_fraction(e) * capacity(e) <= 0,
-        )
+            [direction in (:RampUp, :RampDown), t in ti],
+            (
+                direction === :RampUp ?
+                flow(e, t) - flow(e, timestepbefore(t, 1, sps)) -
+                ramp_up_fraction(e) * capacity(e) :
 
-        @constraint(
-            model,
-            [t in ti],
-            flow(e, timestepbefore(t, 1, sps)) - flow(e, t) -
-            ramp_down_fraction(e) * capacity(e) <= 0,
+                flow(e, timestepbefore(t, 1, sps)) - flow(e, t) -
+                ramp_down_fraction(e) * capacity(e)
+            ) <= 0,
         )
 
     end
@@ -87,26 +86,25 @@ function add_model_constraint!(ct::RampingLimitConstraint, e::EdgeWithUC, model:
     # reserves_term = @expression(model, [t in time_interval(e)], container = array_container(time_interval(e)), 0 * model[:vREF])
     # regulation_term = @expression(model, [t in time_interval(e)], container = array_container(time_interval(e)), 0 * model[:vREF])
 
-    @constraint(
+    ct.constraint_ref = @constraint(
         model,
-        [t in ti],
-        flow(e, t) - flow(e, timestepbefore(t, 1, sps)) - (
-            ramp_up_fraction(e) * capacity_size(e) * (ucommit(e, t) - ustart(e, t)) +
-            min(availability(e, t), max(min_flow_fraction(e), ramp_up_fraction(e))) *
-            capacity_size(e) *
-            ustart(e, t) - min_flow_fraction(e) * capacity_size(e) * ushut(e, t)
-        ) <= 0,
-    )
+        [direction in (:RampUp, :RampDown), t in ti],
+        (
+            direction === :RampUp ?
+            flow(e, t) - flow(e, timestepbefore(t, 1, sps)) - (
+                ramp_up_fraction(e) * capacity_size(e) * (ucommit(e, t) - ustart(e, t)) +
+                min(availability(e, t), max(min_flow_fraction(e), ramp_up_fraction(e))) *
+                capacity_size(e) *
+                ustart(e, t) - min_flow_fraction(e) * capacity_size(e) * ushut(e, t)
+            ) :
 
-    @constraint(
-        model,
-        [t in ti],
-        flow(e, timestepbefore(t, 1, sps)) - flow(e, t) - (
-            ramp_down_fraction(e) * capacity_size(e) * (ucommit(e, t) - ustart(e, t)) -
-            min_flow_fraction(e) * capacity_size(e) * ustart(e, t) +
-            min(availability(e, t), max(min_flow_fraction(e), ramp_down_fraction(e))) *
-            capacity_size(e) *
-            ushut(e, t)
+            flow(e, timestepbefore(t, 1, sps)) - flow(e, t) - (
+                ramp_down_fraction(e) * capacity_size(e) * (ucommit(e, t) - ustart(e, t)) -
+                min_flow_fraction(e) * capacity_size(e) * ustart(e, t) +
+                min(availability(e, t), max(min_flow_fraction(e), ramp_down_fraction(e))) *
+                capacity_size(e) *
+                ushut(e, t)
+            )
         ) <= 0,
     )
     return nothing
